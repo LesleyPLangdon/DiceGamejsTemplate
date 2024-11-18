b8990d7a0d58234b7589cc92a79a06ea26cd14d1
Lesley Potts-Langdon <61936741+LesleyPLangdon@users.noreply.github.com>
2024-11-18 11:41:50 -0500

    function stubs

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 
+/*********** function to generate random dice rolls ***************/
+rollDice() {
+
+  
+}
+
+
+/*********** function to calculate the result, determine winner, and update the score ***************/
+calculateScore() {
+
+}
+
+
+
+/*********** function to display the current roll and scores ***************/
+displayResults() {
+
+}
+
+
