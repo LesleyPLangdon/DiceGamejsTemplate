f22e4528b7bb7fe6dff1228def960b4ec48a3ae8
Lesley Potts-Langdon <61936741+LesleyPLangdon@users.noreply.github.com>
2024-11-21 16:21:27 +0000

    Added html and variables

--- script.js
+++ script.js
@@ -1,20 +1,30 @@
+let playerRoll1 = 0;
+let playerRoll2 = 0;
+let computerRoll1 = 0;
+let computerRoll2 = 0;
+let playerTotal = 0;
+let computerTotal = 0;
+let playerScore = 0;
+let computerScore = 0;
+
+document.getElementById('rollBtn').innerHTML = 
 
 /*********** function to generate random dice rolls ***************/
-rollDice() {
+function rollDice() {
 
   
 }
 
 
 /*********** function to calculate the result, determine winner, and update the score ***************/
-calculateScore() {
+function calculateScore() {
 
 }
 
 
 
 /*********** function to display the current roll and scores ***************/
-displayResults() {
+function displayResults() {
 
 }
 
