5aec0950477c97e8daf61def697678599753d74a
Lesley Potts-Langdon <61936741+LesleyPLangdon@users.noreply.github.com>
2024-12-02 16:40:27 +0000

    added HTML

--- script.js
+++ script.js
@@ -7,44 +7,19 @@ let computerTotal = 0;
 let playerScore = 0;
 let computerScore = 0;
 
-document.getElementById('rollButton').addEventListener('click', rollDice);
+
 
 /*********** function to generate random dice rolls ***************/
 function rollDice() {
-  playerRoll1 = Math.floor(Math.random() * 6) +1;
-  playerRoll2 = Math.floor(Math.random() * 6) +1;
-  computerRoll1 = Math.floor(Math.random() * 6) +1;
-  computerRoll2 = Math.floor(Math.random() * 6) +1;
-  document.getElementById("playerDice").innerHTML = `Player rolled a ${playerRoll1} and a ${playerRoll2}.`;
-  document.getElementById("computerDice").innerHTML = `Computer rolled a ${computerRoll1} and a ${computerRoll2}.`;
-  calculateScore();
+  
 }
-
 /*********** function to calculate the result, determine winner, and update the score ***************/
 function calculateScore() {
-    playerTotal = playerRoll1 + playerRoll2;
-    computerTotal = computerRoll1 + computerRoll2;
-    document.getElementById("playerRoll").innerHTML = `Player Roll: ${playerTotal}`;
-    document.getElementById("computerRoll").innerHTML = `Computer Roll: ${computerTotal}`;
-    displayResults();
+   
 }
-
 /*********** function to display the current roll and scores ***************/
 function displayResults() {
-    if (playerTotal > computerTotal) {
-        document.getElementById("gameResult").innerHTML = "Player wins!";
-        playerScore += 1;
-    } else if (playerTotal < computerTotal) {
-        document.getElementById("gameResult").innerHTML = "Computer wins!";
-        computerScore += 1;
-    } else {
-        document.getElementById("gameResult").innerHTML = "It's a Tie!";
-    }
-    document.getElementById("playerScore").innerHTML = `Player Score: ${playerScore}`;
-    document.getElementById("computerScore").innerHTML = `Computer Score: ${computerScore}`;
-
     
-
 }
 
 
