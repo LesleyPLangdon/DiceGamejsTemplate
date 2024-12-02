d2c9aef8d0d86cf2b463bc3c15fa305134234d73
Lesley Potts-Langdon <61936741+LesleyPLangdon@users.noreply.github.com>
2024-12-02 14:02:35 +0000

    functions rollDice and calculateScore

--- script.js
+++ script.js
@@ -7,16 +7,26 @@ let computerTotal = 0;
 let playerScore = 0;
 let computerScore = 0;
 
-document.getElementById('rollBtn').addEventListener('click', rollDice);
+document.getElementById('rollButton').addEventListener('click', rollDice);
 
 /*********** function to generate random dice rolls ***************/
 function rollDice() {
-  
+  playerRoll1 = Math.floor(Math.random() * 6) +1;
+  playerRoll2 = Math.floor(Math.random() * 6) +1;
+  computerRoll1 = Math.floor(Math.random() * 6) +1;
+  computerRoll2 = Math.floor(Math.random() * 6) +1;
+  document.getElementById("playerDice").innerHTML = `Player rolled a ${playerRoll1} and a ${playerRoll2}.`;
+  document.getElementById("computerDice").innerHTML = `Computer rolled a ${computerRoll1} and a ${computerRoll2}.`;
+  calculateScore();
 }
 
 
 /*********** function to calculate the result, determine winner, and update the score ***************/
 function calculateScore() {
+    playerTotal = playerRoll1 + playerRoll2;
+    computerTotal = computerRoll1 + computerRoll2;
+    document.getElementById("playerRoll").innerHTML = `Player Roll: ${playerTotal}`;
+    document.getElementById("computerRoll").innerHTML = `Computer Roll: ${computerTotal}`;
    
 }
 
