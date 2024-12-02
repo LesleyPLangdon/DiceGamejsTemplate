9713a86257c1f866cc1213ff35a6b211ad283786
Lesley Potts-Langdon <61936741+LesleyPLangdon@users.noreply.github.com>
2024-12-02 16:57:51 +0000

    function display results

--- script.js
+++ script.js
@@ -17,14 +17,30 @@ function rollDice() {
   computerRoll2 = Math.floor(Math.random() * 6) + 1;
   document.getElementById('playerDice').innerHTML = `Player rolled a ${playerRoll1} and a ${playerRoll2}`;
   document.getElementById('computerDice').innerHTML = `Computer rolled a ${computerRoll1} and a ${computerRoll2}`;
+  calculateScore();
 }
 /*********** function to calculate the result, determine winner, and update the score ***************/
 function calculateScore() {
-   
+    playerTotal = playerRoll1 + playerRoll2;
+    computerTotal = computerRoll1 + computerRoll2;
+    document.getElementById('playerRoll').innerHTML = `Player roll: ${playerTotal}`;
+    document.getElementById('computerRoll').innerHTML = `Computer rol: ${computerTotal}`;
+    displayResults();
 }
 /*********** function to display the current roll and scores ***************/
 function displayResults() {
-    
+    if (playerTotal > computerTotal) {
+        document.getElementById('gameResult').innerHTML = "Player Wins!!!"
+        playerScore += 1;
+    } else if (playerTotal < computerTotal) {
+        document.getElementById('gameResult').innerHTML = "Computer Wins!!!"
+        computerScore += 1;
+    } else {
+        document.getElementById('gameResult').innerHTML = "It's a Tie!!!"
+    }
+    document.getElementById("playerScore").innerHTML = `Player score: ${playerScore}`;
+    document.getElementById("computerScore").innerHTML = `Computer score: ${computerScore}`;
+
 }
 
 
